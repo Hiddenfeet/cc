6e1302e67b8aad7fb2338a5f3ec2152ec620c98d
Liquorhands <breyerkevin2019@gmail.com>
2023-07-30 20:32:54 +0200

    1

--- webpack---_N_E/src/config/constants/supportChains.ts
+++
@@ -1,6 +0,0 @@
-import { ChainId } from '@pancakeswap/sdk'
-
-export const SUPPORT_ONLY_BSC = [ChainId.CRONOS]
-export const SUPPORT_FARMS = [ChainId.BSC, ChainId.BSC_TESTNET, ChainId.GOERLI, ChainId.CRONOS]
-
-export const SUPPORT_ZAP = [ChainId.BSC, ChainId.BSC_TESTNET, ChainId.CRONOS]
--- webpack---_N_E/src/utils/verifyBscNetwork.ts
+++
@@ -1,5 +0,0 @@
-import { ChainId } from '@pancakeswap/sdk'
-
-export const verifyBscNetwork = (chainId: number) => {
-  return chainId === ChainId.CRONOS || chainId === ChainId.BSC_TESTNET
-}
